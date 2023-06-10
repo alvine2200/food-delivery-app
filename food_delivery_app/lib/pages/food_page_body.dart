1f0bb49ab7394d26440a2ececfcd96ccdd4cf779
alvine2200 <alvinellavu@gmail.com>
2023-06-10 19:45:50 +0300

    pictures

--- food_delivery_app/lib/pages/food_page_body.dart
+++ food_delivery_app/lib/pages/food_page_body.dart
@@ -14,8 +14,8 @@ class _FoodPageState extends State<FoodPage> {
       height: MediaQuery.of(context).size.height * 0.45,
       child: PageView.builder(
           itemCount: 5,
-          itemBuilder: (context, position) {
-            return _buildPageItem(position);
+          itemBuilder: (context, index) {
+            return _buildPageItem(index);
           }),
     );
   }
@@ -25,9 +25,11 @@ class _FoodPageState extends State<FoodPage> {
       height: 220,
       margin: const EdgeInsets.only(left: 5, right: 5),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
-      ),
+          borderRadius: BorderRadius.circular(30),
+          color:
+              index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
+          image: const DecorationImage(
+              image: AssetImage('assets/image/food1.png'))),
     );
   }
 }
