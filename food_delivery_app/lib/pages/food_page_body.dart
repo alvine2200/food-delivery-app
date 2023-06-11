48b66d6588cadd55c05b8fd1623029e0f75ac151
alvine2200 <alvinellavu@gmail.com>
2023-06-11 10:51:33 +0300

    food page alignment

--- food_delivery_app/lib/pages/food_page_body.dart
+++ food_delivery_app/lib/pages/food_page_body.dart
@@ -11,7 +11,8 @@ class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.45,
+      color: Colors.red,
+      height: 320,
       child: PageView.builder(
           itemCount: 5,
           itemBuilder: (context, index) {
@@ -21,15 +22,34 @@ class _FoodPageState extends State<FoodPage> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: const EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color:
-              index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
-          image: const DecorationImage(
-              image: AssetImage('assets/image/food1.png'))),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 8, right: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven
+                ? const Color(0xFF69c5df)
+                : const Color(0xFF9294cc),
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/food.jpeg'),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: const EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
